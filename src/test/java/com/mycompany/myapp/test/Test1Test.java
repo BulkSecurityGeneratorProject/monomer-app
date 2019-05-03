29a69241dca9eb736f9212dab801a0bafc9d4ed8
tanjie <tanjie@deepexi.com>
2019-05-03 17:57:09 +0800

    添加测试类

---
+++ src/test/java/com/mycompany/myapp/test/Test1Test.java
@@ -0,0 +1,16 @@
+package com.mycompany.myapp.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test1Test {
+
+	@Test
+	public void testTest() {
+		// fail("Not yet implemented");
+		boolean a = true;
+		assertEquals(true, a);
+	}
+
+}
