d0fb128d5c1377eb53a592a0f1c25e4fbc8fb0af
tanjie <tanjie@deepexi.com>
2019-05-03 18:00:40 +0800

    添加单元测试

---
+++ src/test/java/com/mycompany/myapp/test/TestTest.java
@@ -0,0 +1,15 @@
+package com.mycompany.myapp.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTest {
+
+	@Test
+	public void testTest() {
+		int c = 1;
+		assertEquals(c, 1);
+	}
+
+}
