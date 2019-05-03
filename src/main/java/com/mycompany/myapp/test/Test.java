25ad6104ab52bd8af847806331bf4613713601e3
tanjie <tanjie@deepexi.com>
2019-05-03 17:28:01 +0800

    添加一个含有bug的程序

---
+++ src/main/java/com/mycompany/myapp/test/Test.java
@@ -0,0 +1,11 @@
+package com.mycompany.myapp.test;
+
+public class Test {
+	public void test() {
+		int a = 1;
+		int b = 0;
+		int c;
+		c = a / b;// 被除数不能为零
+		System.out.println("c's result is " + c);
+	}
+}
